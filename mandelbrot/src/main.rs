ec60166a7c162bffd965a93f7489054e2e213a81
drshivago <moessner2015@gmail.com>
2018-09-29 09:58:43 -0400

    First Commit

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,12 @@
+extern crate num;
+
+use num::Complex
+
+#[allow(dead_code)]
+
+fn complex_square_add_loop(c: complex<f64>) {
+    let mut z = Complex{re: 0.0, im: 0.0};
+    loop{
+        z = z * z + c;
+    }
+}
